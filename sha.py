6b45caa3f0a877bbb96d2938718b83b49a281720
Binahaagit <binahamary@gmail.com>
2026-05-18 12:06:44 +0530

    added sha

---
+++ sha.py
@@ -0,0 +1,14 @@
+import hashlib
+
+s1 = input("Enter string: ")
+s2 = input("Enter string: ")
+
+h1 = hashlib.sha256(s1.encode()).hexdigest()
+h2 = hashlib.sha256(s2.encode()).hexdigest()
+
+print("Hash 1: ",h1)
+print("Hash 2: ",h2)
+
+x = int(h1,16) ^ int(h2,16)
+
+print("Different bits: ", bin(x).count('1'))
